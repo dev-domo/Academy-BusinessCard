fcb5c8b2768f3bb0ae54c1f942dbff7208c07227
Dustin <gjtmdwns0815@naver.com>
2026-04-26 22:37:08 +0900

    test: #5 전화번호가 올바른 형식인 경우에 대한 테스트 코드 작성

--- Challenge2-BusinessCardTests/PhoneNumberTests.swift
+++ Challenge2-BusinessCardTests/PhoneNumberTests.swift
@@ -20,9 +20,18 @@ struct PhoneNumberTests {
     }
     
     @Test("전화번호 유효성 테스트", arguments: ["0101234567", "010123456789"])
-    func 전화번호_010_뒷자리가_8자리가_아닌_경우_PhoneNumber_객체는_생성되지_않는다(string: String) {        
+    func 전화번호_010_뒷자리가_8자리가_아닌_경우_PhoneNumber_객체는_생성되지_않는다(string: String) {
         let phoneNumber = PhoneNumber(string: string)
         
         #expect(phoneNumber == nil)
+    }
+    
+    @Test
+    func 전화번호가_010으로_시작하고_뒷자리가_8자리인_경우_PhoneNumber_객체가_생성된다() {
+        let string = "01012345678"
+        
+        let phoneNumber = PhoneNumber(string: string)
+        
+        #expect(phoneNumber != nil)
     }
 }
